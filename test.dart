4a2947382b8e177ef057a89f4216839fb30bfd4e
sithsovannita <nita43198@gmail.com>
2025-01-18 11:19:11 +0700

    first_project

---
+++ test.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+void main(){
+  int? code;
+  String? name;
+  String? gender;
+  double? salary;
+
+  stdout.write("Enter code:");code=int.parse(stdin.readLineSync()!);
+  stdout.write("Enter name:");name=stdin.readLineSync();
+  stdout.write("Enter gender:");gender=stdin.readLineSync();
+  stdout.write("Enter salary:");salary=double.parse(stdin.readLineSync()!);
+  print("Code :$code \t Name :$name \t Gender :$gender \t Salary :$salary \t");
+}
